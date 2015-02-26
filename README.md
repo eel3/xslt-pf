eb4e67c18ca15f98f53ab25e0ac9b6746f8b03d7
eel3 <eel3@users.noreply.github.com>
2015-02-27 00:41:36 +0900

    Add -e option

--- README.md
+++ README.md
@@ -23,12 +23,12 @@ for Java implementation:
 1. Install JDK and JRE.
 2. (Mac OS X only) Install realpath from [eel3-scripts](https://github.com/eel3/eel3-scripts "eel3-scripts repository").
 3. Compile XSLTPFCore.java.
-4. Put both xsltpf and XSLTPFCore.class in a directory registered in PATH.
+4. Put xsltpf, XSLTPFCore.class, and XSLTPFCore$XSLInputType.class in a directory registered in PATH.
 5. (Cygwin only) Put xsltpf.bat in a directory registered in PATH (optional).
 
 for Groovy implementation:
 
-1. Install Groovy 2.3.3 or later.
+1. Install Groovy 2.4.1 or later.
 2. Put xsltpf in a directory registered in PATH.
 3. (Windows only) Put xsltpf.bat in a directory registered in PATH.
 
@@ -47,6 +47,30 @@ xsltpf -t foo.xsl -o foo.html foo.xml
 # Transform foo.xml, output to less(1).
 xsltpf -t foo.xsl foo.xml | less
 
-# Transform some command result
+# Transform some command result.
 some-command | xsltpf -t foo.xsl | less
+
+# Use -e option.
+echo '<?xml version="1.0" encoding="utf-8" ?>
+<root>
+  <leaf>1</leaf>
+  <leaf>2</leaf>
+  <leaf>3</leaf>
+</root>
+' | xsltpf -e '<?xml version="1.0" encoding="utf-8" ?>
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+  <xsl:output method="text" encoding="utf-8" />
+  <xsl:template match="/">
+    <xsl:text>[</xsl:text>
+    <xsl:apply-templates select="//leaf" />
+    <xsl:text>]&#x0A;</xsl:text>
+  </xsl:template>
+  <xsl:template match="leaf">
+    <xsl:value-of select="." />
+    <xsl:if test="position() != last()">
+      <xsl:text>, </xsl:text>
+    </xsl:if>
+  </xsl:template>
+</xsl:stylesheet>'
+#=> [1, 2, 3]
 ```
--- java/XSLTPFCore.java
+++ java/XSLTPFCore.java
@@ -3,9 +3,23 @@ import javax.xml.transform.*;
 import javax.xml.transform.stream.*;
 
 public class XSLTPFCore {
+	private enum XSLInputType {
+		STRING, FILE, INVALID,
+	}
+
 	private static void usage() {
-		System.err.println("usage: java XSLTPFCore <-|input-xml> <input-xsl> <-|output>");
+		System.err.println("usage: java XSLTPFCore <-|input-xml> <-e string-xsl|-t input-xsl> <-|output>");
 		System.exit(1);
+	}
+
+	private static XSLInputType xslInputType(String opt) {
+		if (opt.equals("-e")) {
+			return XSLInputType.STRING;
+		} else if (opt.equals("-t")) {
+			return XSLInputType.FILE;
+		} else {
+			return XSLInputType.INVALID;
+		}
 	}
 
 	private static boolean isStream(String file) {
@@ -13,16 +27,22 @@ public class XSLTPFCore {
 	}
 
 	public static void main(String args[]) throws Exception {
-		if (args.length != 3) {
+		if (args.length != 4) {
+			usage();
+		}
+
+		XSLInputType inputType = xslInputType(args[1]);
+		if (inputType == XSLInputType.INVALID) {
 			usage();
 		}
 
 		try {
 			StreamSource in = isStream(args[0]) ? new StreamSource(System.in)
 			                                    : new StreamSource(new File(args[0]));
-			StreamSource ss = new StreamSource(new File(args[1]));
-			StreamResult out = isStream(args[2]) ? new StreamResult(System.out)
-			                                     : new StreamResult(new File(args[2]));
+			StreamSource ss = (inputType == XSLInputType.STRING) ? new StreamSource(new StringReader(args[2]))
+			                                                     : new StreamSource(new File(args[2]));
+			StreamResult out = isStream(args[3]) ? new StreamResult(System.out)
+			                                     : new StreamResult(new File(args[3]));
 
 			TransformerFactory tff = TransformerFactory.newInstance();
 			Transformer tf = tff.newTransformer(ss);
